8273e970efba90e0f819fd2064efdc6be3f85247
Santeri Louna <santeri.louna@hotmail.fi>
2023-04-05 14:17:01 +0300

    t1 ok

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const http = require('http')
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const cors = require('cors')
@@ -11,10 +12,10 @@ const blogSchema = mongoose.Schema({
   likes: Number
 })
 
-//'mongodb+srv://Steve:<password>@cluster0.thmalot.mongodb.net/?retryWrites=true&w=majority'
-const Blog = mongoose.model('Blog', blogSchema)
 
-const mongoUrl = 'mongodb://localhost/bloglist'
+const Blog = mongoose.model('Blog', blogSchema)
+//'mongodb://localhost/bloglist'
+const mongoUrl = process.env.MONGODB_URI
 mongoose.connect(mongoUrl)
 
 app.use(cors())
@@ -38,7 +39,7 @@ app.post('/api/blogs', (request, response) => {
     })
 })
 
-const PORT = 3003
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
