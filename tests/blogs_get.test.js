36b78dd1992420b6a87bc79f6765ee0b754ff06d
Santeri Louna <santeri.louna@hotmail.fi>
2023-04-07 13:21:06 +0300

    t4.5 ok? tarkastaa blogien määrän

--- tests/blogs_get.test.js
+++ tests/blogs_get.test.js
@@ -5,11 +5,15 @@ const app = require('../app')
 const api = supertest(app)
 
 test('blogs are returned as json', async () => {
-  await api
-    .get('/api/blogs')
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
-    
+    await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+  })
+test('there is 4 blogs', async () => {
+  const response = await api.get('/api/blogs')
+  console.log(response.body)
+  expect(response.body).toHaveLength(4)
 })
 
 afterAll(() => {
